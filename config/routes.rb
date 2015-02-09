997216dce2af8e8a7b2fe29649e9101ade30807e
Julia Mansson <julia.mansson@btinternet.com>
2015-02-09 13:34:57 +0000

    adds homecontroller with root to home#index

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
   resources :orders
 
   devise_for :users
-  root to: 'home#index'
+ 
+
+  root "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
