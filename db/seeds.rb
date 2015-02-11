0f50bd224b08f437b7d4ec9f5291e650f7cb253e
Julia Mansson <julia.mansson@btinternet.com>
2015-02-11 14:05:37 +0000

    changed text fields on form to collection select with seeded flavour names in dropdown

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+flavours = Flavour.create([{name: 'Vanilla'}, {name: 'Chocolate'}, {name: 'Red Velvet'} ])
