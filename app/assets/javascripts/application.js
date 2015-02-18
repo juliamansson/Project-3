36da060016fe60acd4fe051315db57c1a59eefda
Julia Mansson <julia.mansson@btinternet.com>
2015-02-18 14:20:00 +0000

    removes edit and delete links from orders index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -174,6 +174,17 @@ $(".submit").click(function(){
     direction: "both"
   });
 
+  $(".continue_button").sparkle({
+
+    color: "#FFFFFF",
+    count: 30,
+    overlap: 0,
+    speed: 1,
+    minSize: 4,
+    maxSize: 7,
+    direction: "both"
+  });
+
 
   $("#linkie_about").sparkle({
 
